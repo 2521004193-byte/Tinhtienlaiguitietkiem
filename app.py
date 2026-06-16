966bd028e420e74d0f0de105262f6741f3f8b221
2521004193-byte <2521004193@sv.ufm.edu.vn>
2026-06-16 14:23:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề ứng dụng
-st.title(" APP TÍNH TIỀN GỬI TIẾT KIÊM_ĐỀ TÀI 7_DUY LÊ DIÊM PHƯƠNG")
+st.title(" APP TÍNH TIỀN GỬI TIẾT KIÊM_ĐỀ TÀI 7_DUY LÊ DIỄM PHƯƠNG")
 
 # Nhập dữ liệu
 C = st.number_input(
